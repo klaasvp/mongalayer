d7c29dd9da2228c8cd971a61a478a03ff5a53311
Klaas Van Parys <klaas.vanparys@sumaqua.be>
2025-07-04 14:01:01 +0200

    Added the database tests to the comparison single operators

--- packages/tests/src/schema/filters/operators/comparisonSingle.test.ts
+++ packages/tests/src/schema/filters/operators/comparisonSingle.test.ts
@@ -1,10 +1,13 @@
 import { z } from 'zod';
-import { filterOperatorsSchema } from '../../../../../server/src/actions/schema';
-import { Mongalayer, MongalayerCollection, MongalayerCollections } from '@mongalayer/server';
-import { FilterTest, filterTestsSchema } from '../../../../data/filterTests';
-import { isMongoServerError } from './helper';
+import { filterOperatorsSchema, filterSchema } from '../../../../../server/src/actions/schema';
+import { Mongalayer } from '@mongalayer/server';
+import { exampleObject1, FilterTest } from '../../../../data/filterTest';
+import { getMongaLayerForFilterTest, isMongoServerError } from './helper';
+import { Filter } from 'mongodb';
 
-const operatorsTable = [
+type Operator = "$eq" | "$gt" | "$gte" | "$lt" | "$lte" | "$ne";
+
+const operatorsTable: [Operator][] = [
     ["$eq"],
     ["$gt"],
     ["$gte"],
@@ -25,64 +28,108 @@ const valuesTable = [
     { value: [1, "a", true, null, [1, 2, 3], { key: 'value' }], success: true, message: `should validate with mixed array`},
 ];
 
+const dbTestTables: Record<Operator, { filter: Filter<FilterTest>, success: boolean, message: string }[]> = {
+    $eq: [
+        { filter: { name: { $eq: exampleObject1.name } }, success: true, message: `"name === name" should return _id a`},
+        { filter: { name: { $eq: "" } }, success: false, message: `"name !== ''" should not return anything`}
+    ],
+    $gt: [
+        { filter: { "details.nestedObject.property2": { $gt: exampleObject1.details.nestedObject.property2 - 1 } }, success: true, message: `"prop > prop - 1" should return _id a`},
+        { filter: { "details.nestedObject.property2": { $gt: exampleObject1.details.nestedObject.property2 } }, success: false, message: `"prop > prop" should not return anything`}
+    ],
+    $gte: [
+        { filter: { "details.nestedObject.property2": { $gte: exampleObject1.details.nestedObject.property2 - 1 } }, success: true, message: `"prop >= prop - 1" should return _id a`},
+        { filter: { "details.nestedObject.property2": { $gte: exampleObject1.details.nestedObject.property2 } }, success: true, message: `"prop >= prop" should return _id a`},
+        { filter: { "details.nestedObject.property2": { $gte: exampleObject1.details.nestedObject.property2 + 1 } }, success: false, message: `"prop >= prop + 1" should not return anything`}
+    ],
+    $lt: [
+        { filter: { "details.nestedObject.property2": { $lt: exampleObject1.details.nestedObject.property2 + 1 } }, success: true, message: `"prop < prop + 1" should return _id a`},
+        { filter: { "details.nestedObject.property2": { $lt: exampleObject1.details.nestedObject.property2 } }, success: false, message: `"prop < prop" should not return anything`}
+    ],
+    $lte: [
+        { filter: { "details.nestedObject.property2": { $lte: exampleObject1.details.nestedObject.property2 + 1 } }, success: true, message: `"prop <= prop - 1" should return _id a`},
+        { filter: { "details.nestedObject.property2": { $lte: exampleObject1.details.nestedObject.property2 } }, success: true, message: `"prop <= prop" should return _id a`},
+        { filter: { "details.nestedObject.property2": { $lte: exampleObject1.details.nestedObject.property2 - 1 } }, success: false, message: `"prop <= prop + 1" should not return anything`}
+    ],
+    $ne: [
+        { filter: { name: { $ne: exampleObject1.name } }, success: false, message: `"name === name" should not return anything`},
+        { filter: { name: { $ne: "" } }, success: true, message: `"name !== ''" should return _id a`}
+    ]
+};
+
 describe('filter operators - Comparison single', () => {
     let mongalayer: Mongalayer;
 
     beforeAll(async () => {
-        const filterTestsCollection: MongalayerCollection<FilterTest> = {
-            schema: filterTestsSchema,
-            access: []
-        };
-
-        const collections: MongalayerCollections = {
-            filterTests: filterTestsCollection
-        }
-
-        mongalayer = new Mongalayer(globalThis.$mdb.client, collections, {
-            //debugging: true,
-            useSessions: true
-        });
+        mongalayer = getMongaLayerForFilterTest();
     });
 
     describe.each(operatorsTable)('%s', ($operator) => {
-        test.each(valuesTable)('$message', async ({value, success, message}) => {
-            const operator = { [$operator]: value };
+        describe('validation', () => {
+            test.each(valuesTable)('$message', async ({value, success, message}) => {
+                const operator = { [$operator]: value };
 
-            const zodResult = filterOperatorsSchema.safeParse(operator);
+                const zodResult = filterOperatorsSchema.safeParse(operator);
 
-            if (success) { 
+                if (success) { 
+                    expect(zodResult.success).toBe(true);
+                } else {
+                    expect(zodResult.success).toBe(false);
+                    expect(zodResult.error!.issues[0]).toHaveProperty('code', z.ZodIssueCode.invalid_type);
+                }
+
+                try { 
+                    const mongaResult = await mongalayer.execute<FilterTest>({
+                        database: globalThis.$mdb.db,
+                        collection: "schemaTest",
+                        operation: "findOne",
+                        payload: {
+                            filter: {
+                                property: operator
+                            }
+                        }
+                    }, {});
+
+                    if (success) {
+                        expect(mongaResult).toBeNull();
+                    } else {
+                        throw "mongalayer.execute should have thrown an error";
+                    }
+                } catch (e) {
+                    if (!success && isMongoServerError(e)) {
+                        expect(e.code).toBe(2);
+                        expect(e.codeName).toBe('BadValue');
+                        expect(e.message).toBe(`${$operator} XXX`);
+                    } else {
+                        throw e;
+                    }
+                }
+            });
+        });
+        
+        describe('on database', () => {
+            test.each(dbTestTables[$operator])('$message', async ({ filter, success }) => {
+                const zodResult = filterSchema.safeParse(filter);
+    
+                // Database tests should always be a valid schema
                 expect(zodResult.success).toBe(true);
-            } else {
-                expect(zodResult.success).toBe(false);
-                expect(zodResult.error!.issues[0]).toHaveProperty('code', z.ZodIssueCode.invalid_type);
-            }
-
-            try { 
+    
                 const mongaResult = await mongalayer.execute<FilterTest>({
                     database: globalThis.$mdb.db,
-                    collection: "filterTests",
+                    collection: "filterTestSolo",
                     operation: "findOne",
                     payload: {
-                        filter: {
-                            property: operator
-                        }
+                        filter
                     }
                 }, {});
-
+    
                 if (success) {
+                    expect(mongaResult).toBeDefined();
+                    expect(mongaResult).toHaveProperty('_id', 'a');
+                } else {
                     expect(mongaResult).toBeNull();
-                } else {
-                    throw "mongalayer.execute should have thrown an error";
                 }
-            } catch (e) {
-                if (!success && isMongoServerError(e)) {
-                    expect(e.code).toBe(2);
-                    expect(e.codeName).toBe('BadValue');
-                    expect(e.message).toBe(`${$operator} XXX`);
-                } else {
-                    throw e;
-                }
-            }
+            });
         });
     });
 });
