f58fb0c47e26eb5362d14a62a061706df1b8ac44
Klaas Van Parys <klaas.vanparys@sumaqua.be>
2025-07-04 12:11:28 +0200

    Added a generic function for getting the monga layer database

--- packages/tests/src/schema/filters/operators/helper.ts
+++ packages/tests/src/schema/filters/operators/helper.ts
@@ -1,5 +1,23 @@
 import { MongoServerError } from "mongodb";
+import { Mongalayer, MongalayerCollection, MongalayerCollections } from "@mongalayer/server";
+import { FilterTest, filterTestsSchema } from "../../../../data/filterTest";
+import { SchemaTest, schemaTestSchema } from "../../../../data/schemaTest";
 
 export const isMongoServerError = (e: any): e is MongoServerError => {
     return Object.prototype.toString.call(e) === '[object Error]' && e.name === "MongoServerError"
 }; 
+
+export const getMongaLayerForFilterTest = () => {
+    const filterTestCollection: MongalayerCollection<FilterTest> = { schema: filterTestsSchema, access: [] };
+    const schemaCollection: MongalayerCollection<SchemaTest> = { schema: schemaTestSchema, access: [] };
+
+    const collections: MongalayerCollections = {
+        filterTest: filterTestCollection,
+        schema: schemaCollection
+    }
+
+    return new Mongalayer(globalThis.$mdb.client, collections, {
+        //debugging: true,
+        useSessions: true
+    });
+}
