1edd0e1e5cb77f021a27f3c846d9b3e04fd8a055
Klaas Van Parys <klaas.vanparys@sumaqua.be>
2025-07-08 09:47:12 +0200

    Added the logical operators

---
+++ packages/tests/src/schema/filters/operators/logical.test.ts
@@ -0,0 +1,145 @@
+import { z } from 'zod/v4';
+import { filterOperatorsSchema, filterSchema } from '../../../../../server/src/actions/schema';
+import { Mongalayer } from '@mongalayer/server';
+import { exampleObject1, FilterTest } from '../../../../data/filterTest';
+import { getMongaLayerForFilterTest, isMongoServerError, isZodError, MongoDBException, ZodException } from '../helper';
+import { Filter } from 'mongodb';
+import { SchemaTest } from '../../../../data/schemaTest';
+
+type Operator = "$and" | "$or" | "$nor";
+
+const operatorsTable: [Operator][] = [
+    ["$and"],
+    ["$or"],
+    ["$nor"]
+];
+
+const valuesTable: { value: any, success: boolean, message: string, exceptions?: { zod?: ZodException, mongodb?: MongoDBException } }[] = [
+    { value: [{ name: 'John' }, { age: 30 }], success: true, message: `should validate with array of objects`},
+    { value: [{ valid: true }], success: true, message: `should validate with array of a object`},
+    { value: [], success: false, message: `should invalidate with empty array`, exceptions: {
+        mongodb: { code: 2, codeName: "BadValue", message: "argument must be a non-empty array" },
+        zod: { code: "too_small", message: 'Too small: expected array to have >=1 items' }
+    }},
+    { value: [{ $and: [{ name: 'John' }, { age: 30 }] }], success: true, message: `should validate with nested $and array of objects`},
+    { value: [{ $or: [{ name: 'John' }, { age: 30 }] }], success: true, message: `should validate with nested $or array of objects`},
+    { value: [{ $nor: [{ name: 'John' }, { age: 30 }] }], success: true, message: `should validate with nested $nor array of objects`},
+    { value: [{ valid: { $and: [{ name: 'John' }, { age: 30 }] } }], success: false, message: `should invalidate with $and as operator`, exceptions: {
+        mongodb: { code: 2, codeName: "BadValue", message: "unknown operator: " },
+        zod: { code: "invalid_union", message: 'Invalid input' }
+    }},
+    { value: 'invalid', success: false, message: `should invalidate with string`, exceptions: {
+        mongodb: { code: 2, codeName: "BadValue", message: "argument must be an array" },
+        zod: { code: "invalid_type", message: 'Invalid input: expected array, received string' }
+    }},
+    { value: null, success: false, message: `should invalidate with null`, exceptions: {
+        mongodb: { code: 2, codeName: "BadValue", message: "argument must be an array" },
+        zod: { code: "invalid_type", message: 'Invalid input: expected array, received null' }
+    }},
+    { value: 123, success: false, message: `should invalidate with number`, exceptions: {
+        mongodb: { code: 2, codeName: "BadValue", message: "argument must be an array" },
+        zod: { code: "invalid_type", message: 'Invalid input: expected array, received number' }
+    }},
+    { value: true, success: false,  message: `should invalidate with boolean`, exceptions: {
+        mongodb: { code: 2, codeName: "BadValue", message: "argument must be an array" },
+        zod: { code: "invalid_type", message: 'Invalid input: expected array, received boolean' }
+    }},
+    { value: {}, success: false, message: `should invalidate with object`, exceptions: {
+        mongodb: { code: 2, codeName: "BadValue", message: "argument must be an array" },
+        zod: { code: "invalid_type", message: 'Invalid input: expected array, received object' }
+    }}
+];
+
+const dbTestTables: Record<Operator, { filter: Filter<FilterTest>, success: boolean, message: string }[]> = {
+    $and: [
+        { filter: { $and: [{ name: exampleObject1.name }, { status: exampleObject1.status }] }, success: true, message: `"$and with valid conditions" should return _id a`},
+        { filter: { $and: [{ name: "" }, { status: exampleObject1.status }] }, success: false, message: `"$and with invalid conditions" should not return anything`}
+    ],
+    $or: [
+        { filter: { $or: [{ name: exampleObject1.name }, { status: exampleObject1.status }] }, success: true, message: `"$or with 2 valid conditions" should return _id a`},
+        { filter: { $or: [{ name: exampleObject1.name }, { status: !exampleObject1.status }] }, success: true, message: `"$or with 1 valid conditions" should return _id a`},
+        { filter: { $or: [{ name: "" }, { status: !exampleObject1.status }] }, success: false, message: `"$or with invalid conditions" should not return anything`}
+    ],
+    $nor: [
+        { filter: { $nor: [{ name: exampleObject1.name }, { status: exampleObject1.status }] }, success: false, message: `"$nor with 2 valid conditions" should not return anything`},
+        { filter: { $nor: [{ name: "" }, { status: exampleObject1.status }] }, success: false, message: `"$nor with 1 valid condition" should not return anything`},
+        { filter: { $nor: [{ name: "" }, { status: !exampleObject1.status }] }, success: true, message: `"$nor with invalid conditions" should return _id a`}
+    ]
+};
+
+describe('filter operators - Logical', () => {
+    let mongalayer: Mongalayer, database = globalThis.$mdb.db;
+
+    beforeAll(async () => {
+        mongalayer = getMongaLayerForFilterTest({ debugging: true });
+    });
+
+    describe.each(operatorsTable)('%s', ($operator) => {
+        describe('validation', () => {
+            test.each(valuesTable)('$message', async ({value, success, message, exceptions}) => {
+                const filter = { [$operator]: value };
+
+                const zodResult = filterSchema.safeParse(filter);
+
+                if (success) {
+                    if (!zodResult.success) 
+                        debugger;
+                    expect(zodResult.success).toBe(true);
+                } else {
+                    expect(zodResult.success).toBe(false);
+                    expect(zodResult.error!.issues[0]).toHaveProperty('code', exceptions?.zod?.code);
+                    expect(zodResult.error!.issues[0].message).toBe(exceptions?.zod?.message);
+                }
+
+                try {
+                    const result = await database.collection<SchemaTest>("schemaTest").findOne(filter, {});
+
+                    if (success) {
+                        expect(result).toBeNull();
+                    } else {
+                        throw "mongalayer.execute should have thrown an error";
+                    }
+                } catch (e) {
+                    if (!success && isMongoServerError(e)) {
+                        if (exceptions && exceptions.mongodb) {
+                            expect(e.code).toBe(exceptions.mongodb.code);
+                            expect(e.codeName).toBe(exceptions.mongodb.codeName);
+                            expect(e.message).toContain(exceptions.mongodb.message);
+                        } else {
+                            expect(e.code).toBe(2);
+                            expect(e.codeName).toBe('BadValue');
+                            expect(e.message).toBe(`${$operator} argument must be an array`);
+                        }
+                    } else {
+                        throw e;
+                    }
+                }
+            });
+        });
+
+        describe('on filterTestSolo collection', () => {
+            test.each(dbTestTables[$operator])('$message', async ({ filter, success }) => {
+                const zodResult = filterSchema.safeParse(filter);
+
+                // Database tests should always be a valid schema
+                expect(zodResult.success).toBe(true);
+
+                const mongaResult = await mongalayer.execute<FilterTest>({
+                    database: globalThis.$mdb.name,
+                    collection: "filterTestSolo",
+                    operation: "findOne",
+                    payload: {
+                        filter
+                    }
+                }, {});
+
+                if (success) {
+                    expect(mongaResult).toBeDefined();
+                    expect(mongaResult).toHaveProperty('_id', 'a');
+                } else {
+                    expect(mongaResult).toBeNull();
+                }
+            });
+        });
+    });
+});
