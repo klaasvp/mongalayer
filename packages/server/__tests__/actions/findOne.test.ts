5e3690be12875b382dd8fb0b437ad1e5ce840f4b
Klaas Van Parys <klaas.vanparys@sumaqua.be>
2025-07-23 09:28:28 +0200

    Fixed the findOne test

--- packages/server/__tests__/actions/findOne.test.ts
+++ packages/server/__tests__/actions/findOne.test.ts
@@ -2,19 +2,16 @@ import { describe, expect, test, beforeAll } from "vitest";
 import { Project, projectSchema } from "#test/data/project";
 import { User } from "#test/data/user";
 import { z } from "zod/v4";
-import { ZodInvalidTypeIssue, ZodUnrecognizedKeysIssue } from "zod";
-import { dbName, getMongaLayerForFilterTest, getMongoDBDatabase, projectObjects } from "#test/lib/database";
+import { dbName, getMongaLayerForCollections, getMongoDBDatabase, projectObjects } from "#test/lib/database";
 import { Db } from "mongodb";
 import { Mongalayer, MongalayerCollection, MongalayerCollections } from "#src/core";
+import { $ZodIssueInvalidType, $ZodIssueUnrecognizedKeys } from "zod/v4/core";
 
 describe('Find One', () => {
     let mongalayer: Mongalayer, projectZero: Project, database: Db;
 
     beforeAll(async () => {
-        const projectCollection: MongalayerCollection<Project> = {
-            schema: projectSchema,
-            access: []
-        };
+        const projectCollection: MongalayerCollection<Project> = { schema: projectSchema, access: [] };
 
         const collections: MongalayerCollections = {
             projects: projectCollection
@@ -23,7 +20,7 @@ describe('Find One', () => {
         projectZero = projectObjects[0];
 
         database = await getMongoDBDatabase();
-        mongalayer = await getMongaLayerForFilterTest({ debugging: true });
+        mongalayer = await getMongaLayerForCollections(collections, { debugging: true });
     });
 
     test("filter - _id = _id", async () => {
@@ -75,10 +72,10 @@ describe('Find One', () => {
         } catch (e) {
             expect(e).toBeInstanceOf(z.ZodError);
             
-            let zodError = (e as z.ZodError)
+            const zodError = (e as z.ZodError), zodIssue = (zodError.issues[0] as $ZodIssueUnrecognizedKeys);
             
-            expect(zodError.issues[0].code).toBe("unrecognized_keys");
-            expect((zodError.issues[0] as ZodUnrecognizedKeysIssue).keys).toStrictEqual(["idontexistprop"]);
+            expect(zodIssue.code).toBe("unrecognized_keys");
+            expect(zodIssue.keys).toStrictEqual(["idontexistprop"]);
         }
     });
 
@@ -98,11 +95,10 @@ describe('Find One', () => {
         } catch (e) {
             expect(e).toBeInstanceOf(z.ZodError);
             
-            let zodError = (e as z.ZodError)
+            const zodError = (e as z.ZodError), zodIssue = (zodError.issues[0] as $ZodIssueInvalidType);
             
-            expect(zodError.issues[0].code).toBe("invalid_type");
-            expect((zodError.issues[0] as unknown as ZodInvalidTypeIssue).expected).toBe("string");
-            expect((zodError.issues[0] as unknown as ZodInvalidTypeIssue).received).toBeUndefined();
+            expect(zodIssue.code).toBe("invalid_type");
+            expect(zodIssue.expected).toBe("string");
         }
     });
 });
