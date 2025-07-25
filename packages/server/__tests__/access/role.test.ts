779225e6505ca17720872feb3017a3741f28ef88
Klaas Van Parys <klaas.vanparys@sumaqua.be>
2025-07-25 10:12:59 +0200

    Added a role test

---
+++ packages/server/__tests__/access/role.test.ts
@@ -0,0 +1,121 @@
+import { describe, expect, test, beforeAll } from "vitest";
+import { Mongalayer, MongalayerCollection, MongalayerCollections, MongalayerCollectionType } from "#src/core";
+import { User, userSchema } from "#test/data/user";
+import { Project, projectSchema } from "#test/data/project";
+import { JwtPayload } from "jsonwebtoken";
+import { dbName, getMongaLayerForCollections, getMongoDBDatabase, projectObjects, userObjects } from "#test/lib/database";
+import { AccessConfig, AccessFieldPermissions, AccessService, WithAccessRole } from "#src/access";
+import { Db, Document } from "mongodb";
+import { ZodObject } from "zod/v4";
+
+describe('Access - Roles', () => {
+    let database: Db, userZero = userObjects[0], userOne = userObjects[0];
+
+    beforeAll(async () => {
+        database = await getMongoDBDatabase();
+    });
+
+    test("User - No access - Role empty", async () => {
+        const collection: MongalayerCollection<User> = {
+            schema: userSchema,
+            access: []
+        }
+
+        const accessService = new AccessService({}, collection.access as AccessConfig, collection.schema, AccessFieldPermissions.Read);
+
+        const stages = accessService.getStages({});
+
+        const pipeline: Document[] = [ { $match: {} } ];
+
+        if (stages.$role) pipeline.push({ $addFields: stages.$role });
+
+        pipeline.push({ $limit: 1 });
+        
+        const 
+            results = await database.collection("users").aggregate<WithAccessRole<User>>(pipeline).toArray(),
+            result = results[0];
+
+        expect(result).not.toHaveProperty("__mongalayer_role");
+    });
+
+    test("User - Self access - Role Self", async () => {
+        const collection: MongalayerCollection<User> = {
+            schema: userSchema,
+            access: [{
+                role: "self",
+                filter: {
+                    $eq: ["$_id", "%%user.sub"]
+                }
+            }]
+        }
+
+        const accessService = new AccessService({user: {sub: userZero._id}}, collection.access as AccessConfig, collection.schema, AccessFieldPermissions.Read);
+
+        const stages = accessService.getStages({});
+
+        const pipeline: Document[] = [ { $match: { _id: userZero._id } } ];
+
+        if (stages.$role) pipeline.push({ $addFields: stages.$role });
+
+        pipeline.push({ $limit: 1 });
+        
+        const 
+            results = await database.collection("users").aggregate<WithAccessRole<User>>(pipeline).toArray(),
+            result = results[0];
+
+        expect(result).toBeDefined();
+
+        if (result) {
+            expect(result).toHaveProperty("__mongalayer_role");
+            expect(result.__mongalayer_role).toBe("self");
+        }
+    });
+
+    test("Project - Multiple roles", async () => {
+        const collection: MongalayerCollection<Project> = {
+            schema: projectSchema,
+            access: [{
+                role: "owner",
+                filter: {
+                    "$in": ["%%user.sub", "$access.owners"]
+                }
+            }, {
+                role: "contributor",
+                filter: {
+                    "$in": ["%%user.sub", "$access.contributors"]
+                }
+            }, {
+                role: "reader",
+                filter: {
+                    "$in": ["%%user.sub", "$access.readers"]
+                }
+            }]
+        };
+
+        const projectRoleMapping = projectObjects.reduce((mapping, project) => {
+            if (project.access.owners.includes(userZero._id)) mapping[project._id] = "owner";
+            else if (project.access.contributors.includes(userZero._id)) mapping[project._id] = "contributor";
+            else if (project.access.readers.includes(userZero._id)) mapping[project._id] = "reader";
+            else mapping[project._id] = null;
+
+            return mapping;
+        }, {} as Record<string, string | null>);
+
+        const accessService = new AccessService({user: {sub: userZero._id}}, collection.access as AccessConfig, collection.schema, AccessFieldPermissions.Read);
+
+        const stages = accessService.getStages({});
+
+        const pipeline: Document[] = [ { $match: {} } ];
+
+        if (stages.$role) pipeline.push({ $addFields: stages.$role });
+        
+        const result = await database.collection("projects").aggregate<WithAccessRole<Project>>(pipeline).toArray();
+
+        expect(result.length).toEqual(projectObjects.length);
+
+        result.forEach(project => {
+            expect(project).toHaveProperty("__mongalayer_role");
+            expect(project.__mongalayer_role).toBe(projectRoleMapping[project._id]);
+        });
+    });
+});
