921198733f27b9cee75259dab24584a8d6f116da
Klaas Van Parys <klaas.vanparys@sumaqua.be>
2025-07-28 17:02:43 +0200

    Return the current path & parent instance to the callback

--- packages/server/src/utils/replacer.ts
+++ packages/server/src/utils/replacer.ts
@@ -1,11 +1,11 @@
 import { isArray, isObject } from "./core.js";
 
 type IterateCallbackReplacer = (newValue: any) => void;
-export type IterateCallback = (key: string | number, value: any, replace: IterateCallbackReplacer) => void;
+export type IterateCallback = (key: string | number, value: any, replace: IterateCallbackReplacer, path: string[], parent: any[] | Record<string, unknown>) => void;
 
 type IteratePrimitives = "string" | "number" | "boolean";
 
-export function iteratePrimitives (instance: any[] | Record<string, unknown>, callback: IterateCallback, primitives: IteratePrimitives[] = ["string", "number", "boolean"]) {
+export function iteratePrimitives (instance: any[] | Record<string, unknown>, callback: IterateCallback, primitives: IteratePrimitives[] = ["string", "number", "boolean"], path: string[] = []) {
     const entries: [string | number, any][] = [];
 
     if (isArray(instance)) {
@@ -16,12 +16,14 @@ export function iteratePrimitives (instance: any[] | Record<string, unknown>, ca
 
     if (entries.length > 0) {
         for (const [key, value] of entries) {
+            const valuePath = [...path, key.toString()];
+
             if ((primitives as string[]).includes(typeof value)) {
                 callback(key, value, (newValue) => {
                     (instance as any)[key] = newValue;
-                });
+                }, valuePath, instance);
             } else if (isArray(value) || isObject(value)) {
-                iteratePrimitives(value, callback, primitives);
+                iteratePrimitives(value, callback, primitives, valuePath);
             }
         }
     }
