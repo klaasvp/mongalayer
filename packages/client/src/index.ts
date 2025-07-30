d6b33ab96c6994bddb6b775db31e852749ae1ce8
Klaas Van Parys <klaas.vanparys@sumaqua.be>
2025-07-30 10:22:50 +0200

    Export the db & collection classes for the client

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -3,3 +3,11 @@ export type * from "@mongalayer/server/client";
 export {
     Client as MongalayerClient
 } from "./client"
+
+export {
+    Db
+} from "./db"
+
+export {
+    Collection
+} from "./collection"
