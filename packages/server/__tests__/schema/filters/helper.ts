4313cc11236151fd9dc47e47af55c074463d2053
Klaas Van Parys <klaas.vanparys@sumaqua.be>
2025-08-04 16:19:51 +0200

    Generalized the helper exceptions

--- packages/server/__tests__/schema/filters/helper.ts
+++ packages/server/__tests__/schema/filters/helper.ts
@@ -2,21 +2,9 @@ import { Document, Filter, MongoAPIError, MongoServerError } from "mongodb";
 import { FilterTest } from "#test/data/filterTest";
 import { z } from "zod/v4";
 import { FilterSchema } from "#src/schema/query.js";
+import { MongoAPIException, MongoDBException, ZodException } from "#test/lib/helper.js";
 
-export type MongoDBException = {
-    code: number,
-    codeName: string,
-    message: string
-};
-
-export type MongoAPIException = {
-    message: string
-};
-
-export type ZodException = {
-    code: string,
-    message: string
-}
+export * from "../../lib/helper.js";
 
 export type ValueTest = ({
     filter: any | Filter<Document>,
@@ -56,15 +44,3 @@ export type DbProjectTest = {
     }, 
     message: string 
 }
-
-export const isMongoServerError = (e: any): e is MongoServerError => {
-    return Object.prototype.toString.call(e) === '[object Error]' && e.name === "MongoServerError"
-}; 
-
-export const isMongoInvalidArgumentError = (e: any): e is MongoAPIError => {
-    return Object.prototype.toString.call(e) === '[object Error]' && e.name === "MongoInvalidArgumentError";
-}; 
-
-export const isZodError = (e: any): e is z.ZodError => {
-    return Object.prototype.toString.call(e) === '[object Error]' && e instanceof z.ZodError;
-}; 
