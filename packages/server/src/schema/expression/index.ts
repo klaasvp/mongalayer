f430c6ab441dc78dd159443098578b8b641a3385
Klaas Van Parys <klaas.vanparys@sumaqua.be>
2025-08-04 16:53:46 +0200

    Added some first aggregation expressions

---
+++ packages/server/src/schema/expression/index.ts
@@ -0,0 +1,59 @@
+import z from "zod"
+
+const inputWithNSchema = z.lazy(() => z.strictObject({ input: expressionSchema, n: expressionSchema }))
+
+export type First = { $first: Expression }
+export const $first: z.ZodType<First> = z.strictObject({ get $first () { return lazyExpressionSchema } })
+
+export type FirstN = { $firstN: { input: Expression, n: Expression } }
+export const $firstN: z.ZodType<FirstN> = z.strictObject({ get $firstN () { return inputWithNSchema } })
+
+export type Last = { $last: Expression }
+export const $last: z.ZodType<Last> = z.strictObject({ get $last () { return lazyExpressionSchema } })
+
+export type LastN = { $lastN: { input: Expression, n: Expression } }
+export const $lastN: z.ZodType<LastN> = z.strictObject({ get $lastN () { return inputWithNSchema } })
+
+export type ExpressionOperator = {
+    $avg: Expression | Expression[]
+} | {
+    $max: Expression | Expression[]
+} | {
+    $median: { input: Expression[], method: "approximate" }
+} | {
+    $min: Expression | Expression[]
+} | {
+    $sum: Expression[]
+} | First | FirstN | Last | LastN
+
+export type Expression = string | ExpressionOperator;
+
+/*export type ExpressionReturnType = z.ZodType<Expression>
+export type ExpressionWithArrayReturnType = z.ZodType<Expression | Expression[]>*/
+
+export const operatorSchema: z.ZodType<ExpressionOperator> = z.union([
+    z.strictObject({
+        get $avg () { return z.lazy(() => expressionSchema.or(expressionSchema.array())) }
+    }),
+    $first,
+    $firstN,
+    $last,
+    $lastN,
+    z.strictObject({
+        get $max () { return z.lazy(() => expressionSchema.or(expressionSchema.array())) }
+    }),
+    z.strictObject({
+        get $median () { return z.lazy(() => z.strictObject({  input: expressionSchema.array(), method: z.literal("approximate") })) }
+    }),
+    z.strictObject({
+        get $min () { return z.lazy(() => expressionSchema.or(expressionSchema.array())) }
+    }),
+    z.strictObject({
+        get $sum () { return z.lazy(() => expressionSchema.array()) }
+    })
+])
+
+// For now we'll only support paths ($...) as expressions next to the operators
+export const expressionSchema: z.ZodType<Expression> = operatorSchema.or(z.string().regex(/^\$/));
+
+const lazyExpressionSchema = z.lazy(() => expressionSchema);
