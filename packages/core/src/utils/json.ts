f3bfec8e2ae3fd4bb6920c8e2edabcb289bdc4d1
Klaas Van Parys <klaas.vanparys@sumaqua.be>
2025-09-30 10:23:21 +0200

    Regex replacer fix

--- packages/core/src/utils/json.ts
+++ packages/core/src/utils/json.ts
@@ -2,7 +2,10 @@ export function stringifyReplacer (this: any, key: string, value: any) {
     if (this[key] instanceof Date) {
         return { __$date: this[key].getTime() };
     } else if (this[key] instanceof RegExp) {
-        return this[key].toString();
+        if (typeof this[key].flags === "string" && this[key].flags.length > 0) {
+            this.$options = this[key].flags;
+        }
+        return this[key].source;
     }
     
     return value;
