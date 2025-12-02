73d61805cb1b0c65d7e0aaed37aa5a074ab2f016
Klaas Van Parys <klaas.vanparys@sumaqua.be>
2025-12-02 11:09:30 +0100

    Expose the access filter type

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,4 +4,5 @@ export * from "./core.js";
 
 export type { MongalayerCollectionType } from "./actions/index.js";
 
+export type { AccessFilter } from "./schema/access/filter.js";
 export { type AccessConfig,  AccessPermissions, type AccessDefinitionFilter, type AccessAlternativeCollection, type CreateAccessValidator, type UpdateAccessValidator, AccessValidatorError, defineUpdateAccessValidator } from "./access.js";
