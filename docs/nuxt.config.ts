ccb8494a9e2ae793f445a7ac1f488c37a1212bbe
Klaas Van Parys <accounts@klaas.cc>
2026-05-26 11:45:39 +0200

    amplify runtime update

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,3 +1,9 @@
 export default defineNuxtConfig({
     extends: ['docus'],
+    nitro: {
+        awsAmplify: {
+            // @ts-expect-error - Nuxt 4 uses Nitro v2 which doesn't support Node 24 yet
+            runtime: 'nodejs24.x',
+        },
+    }
 })
