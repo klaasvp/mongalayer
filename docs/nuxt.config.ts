71e761c1526c656572d37f5348e2b431fbf44ee8
Klaas Van Parys <accounts@klaas.cc>
2026-05-26 15:38:35 +0200

    Disable image provider

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,9 +1,13 @@
 export default defineNuxtConfig({
     extends: ['docus'],
+    image: {
+        provider: "none",
+    },
     nitro: {
         awsAmplify: {
             // @ts-expect-error - Nuxt 4 uses Nitro v2 which doesn't support Node 24 yet
             runtime: 'nodejs24.x',
+
         },
     }
 })
